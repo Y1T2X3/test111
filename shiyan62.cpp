752d893e6762a82d1ab5168484cf80f803d0fcf7
廷相 杨 <2821960017@qq.com>
2024-03-05 19:29:47 +0800

    添加项目文件。

---
+++ shiyan62.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a; cin >> a;
+    for (int i = 0; i < a; i++)
+    {
+        int b, c; cin >> b >> c; int* k = new int[b];
+        for (int j = 0; j < c; j++)
+        {
+            k[j] = 0;
+        }
+        for (int j = 0; j < c; j++)
+        {
+            int d, e; cin >> d >> e;
+            k[e]++;
+        }
+        for (int j = 0; j < c-1; j++)
+        {
+            if (k[j] > k[j + 1])
+            {
+                int temp = k[j];
+                k[j] = k[j + 1];
+                k[j + 1] = temp;
+            }
+        }
+        cout << k[c - 1] << endl;
+    }
+}
